05326e9df0e1727277d4d3c8c5a2477e88980caf
Joey Hipolito <hi@joeyhipolito.com>
2014-03-10 07:57:35 +0800

    Serve vote using a controller

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -19,28 +19,6 @@ angular.module('ellenApp')
       };
     }
 
-    $scope.upvote = function(feedID) {
-      var feed = FeedService.findById(feedID);
-      feed.$on('loaded', function(){
-        if (VoteService.isUpVoted(feed)) {
-          VoteService.clearvote(feed, feedID, true);
-        } else {
-          VoteService.upvote(feed, feedID);
-        }
-      });
-    }
-
-    $scope.downvote = function(feedID) {
-      var feed = FeedService.findById(feedID);
-      feed.$on('loaded', function(){
-        if (VoteService.isDownVoted(feed)) {
-          VoteService.clearvote(feed, feedID, false);
-        } else {
-          VoteService.downvote(feed, feedID);
-        }
-      });
-    }
-
     // modal / toggles
 
     $scope.toggleMenu = function() {

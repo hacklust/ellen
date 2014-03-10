02bae5e739a1835d914380a4d101c4c34a9b3edf
Joey Hipolito <hi@joeyhipolito.com>
2014-03-10 12:34:20 +0800

    Allow article comments and fix no refresh navigation on post per type

--- app/scripts/controllers/articles.js
+++ app/scripts/controllers/articles.js
@@ -9,9 +9,16 @@ angular.module('ellenApp')
     $scope.articles = ArticleService.all;
 
     $scope.articles.$on('loaded', function(ref){
-      angular.forEach(ref, function(a){
-        $scope.feeds.push(FeedService.findById(a.id));
-      });
+      // workaround
+      if (ref === undefined) {
+        angular.forEach(ref, function(a){
+          console.log(a);
+        });
+      } else {
+        angular.forEach(ref, function(a){
+          $scope.feeds.push(FeedService.findById(a.id));
+        });
+      }
     });
 
     $scope.toggleMenu = function() {
--- app/scripts/controllers/questions.js
+++ app/scripts/controllers/questions.js
@@ -6,13 +6,20 @@ angular.module('ellenApp')
     $scope.feedType = 'Questions';
 
     $scope.feeds = [];
-
     $scope.questions = QuestionService.all;
 
     $scope.questions.$on('loaded', function(ref){
-      angular.forEach(ref, function(q){
-        $scope.feeds.push(FeedService.findById(q.id));
-      });
+      // workaround
+      if (ref === undefined) {
+        angular.forEach(ref, function(a){
+          console.log(a);
+        });
+      } else {
+        angular.forEach(ref, function(a){
+          $scope.feeds.push(FeedService.findById(a.id));
+        });
+      }
+
     })
 
     $scope.toggleMenu = function() {

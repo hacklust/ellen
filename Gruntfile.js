8fe98e72c9cc4b96ce741e504318f25e1edba539
Joey Hipolito <hi@joeyhipolito.com>
2014-03-12 03:02:10 +0800

    cordova and grunt file, nothing important

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,12 +62,12 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'joey.dev',
+       hostname: '0.0.0.0',
         livereload: 35729
       },
       livereload: {
         options: {
-          open: 'http://joey.dev:9000',
+          open: true,
           base: [
             '.tmp',
             '<%= yeoman.app %>'
